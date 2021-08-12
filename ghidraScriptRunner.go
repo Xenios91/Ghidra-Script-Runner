c915652fd0b6b95746d742499f86da183771a40e
Corey <xenios91@protonmail.com>
2021-08-12 04:52:42 +0000

    cleanup

--- ghidraScriptRunner.go
+++ ghidraScriptRunner.go
@@ -1,7 +1,6 @@
-package ghidrautils
+package ghidraScriptRunner
 
 import (
-	config "glyph/glyph/config"
 	"log"
 	"os/exec"
 	"path/filepath"
@@ -11,82 +10,47 @@ import (
 type ghidraAnalysisQueue map[string]*ghidraQueueValue
 
 type ghidraQueueValue struct {
-	isTrainingData bool
-	status         *string
-}
-
-type ghidraAnalysisConfig struct {
-	ghidraHeadless        *string
-	ghidraProjectLocation *string
-	ghidraProject         *string
-	ghidraScript          *string
+	script *string
+	status *string
 }
 
 var (
 	once         sync.Once
 	ghidraQueue  ghidraAnalysisQueue
-	ghidraConfig *ghidraAnalysisConfig
+	ghidraConfig *Configuration
 )
 
-func main() {
-	fmt.Println("well, hello there")
-}
-
 //StartGhidraAnalysis Starts analysis on a supplied binary and returns a boolean value indicating if the analysis was successfully started.
-func StartGhidraAnalysis(fileName string, trainingData bool) bool {
-	err := exec.Command(*ghidraConfig.ghidraHeadless, *ghidraConfig.ghidraProjectLocation, *ghidraConfig.ghidraProject, "-import", fileName, "-postScript", *ghidraConfig.ghidraScript, "-overwrite").Start()
+func StartGhidraAnalysis(fileName *string, script *string) error {
+	err := exec.Command(*ghidraConfig.ghidraHeadless, *ghidraConfig.ghidraProjectLocation, *ghidraConfig.ghidraProject, "-import", *fileName, "-postScript", *ghidraConfig.ghidraScript, "-overwrite").Start()
 	if err != nil {
-		log.Println(err)
-		return false
+		return err
 	}
-	addToQueue(filepath.Base(fileName), trainingData)
-	return true
+	addToQueue(filepath.Base(*fileName), script)
+	return nil
 }
 
 //LoadGhidraAnalysis sets all configuration information for ghidra based on arguments supplied.
-func LoadGhidraAnalysis(configuration *config.Configuration) {
+func LoadGhidraAnalysis(configuration *Configuration) {
 	once.Do(func() {
 		log.Print("Loading Ghidra analysis queue... ")
 		ghidraQueue = make(ghidraAnalysisQueue)
-		ghidraConfig = new(ghidraAnalysisConfig)
-		ghidraConfig.ghidraHeadless = configuration.GhidraHeadless
-		ghidraConfig.ghidraProjectLocation = configuration.GhidraProjectLocation
-		ghidraConfig.ghidraProject = configuration.GhidraProject
-		ghidraConfig.ghidraScript = configuration.GhidraScript
+		ghidraConfig = new(Configuration)
 		log.Println("Ghidra Analysis Queue successfully loaded!")
 	})
 }
 
-func addToQueue(binaryName string, trainingData bool) {
+func addToQueue(binaryName string, script *string) {
 	queueValue := new(ghidraQueueValue)
 	beginningStatus := "Waiting on Ghidra"
 	queueValue.status = &beginningStatus
-	queueValue.isTrainingData = trainingData
+	queueValue.script = script
 	ghidraQueue[binaryName] = queueValue
 }
 
 //RemoveFromQueue removes a binary name from the queue of binaries being processed by ghidra.
 func RemoveFromQueue(binaryName string) {
 	delete(ghidraQueue, binaryName)
-}
-
-func checkIfTraining(binaryName *string) bool {
-	var isTraining bool
-	queueValue := ghidraQueue[*binaryName]
-
-	if queueValue != nil {
-		isTraining = queueValue.isTrainingData
-	} else {
-		isTraining = false
-	}
-	return isTraining
-}
-
-//CheckIfTrainingAndRemove returns true/false if a binary being processed by ghidra currently is training data, and removes it from the queue.
-func CheckIfTrainingAndRemove(binaryName string) bool {
-	isTraining := checkIfTraining(&binaryName)
-	RemoveFromQueue(binaryName)
-	return isTraining
 }
 
 //UpdateQueue updates the status of a binary currently in the queue.
---
+++ ghidraUtils.go
@@ -0,0 +1,8 @@
+package ghidraScriptRunner
+
+type Configuration struct {
+	ghidraHeadless        *string
+	ghidraProjectLocation *string
+	ghidraProject         *string
+	ghidraScript          *string
+}
