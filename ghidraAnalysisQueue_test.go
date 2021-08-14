271e524c5c5dd33866dfd38dfa8b403454311398
xenios <xenios91@protonmail.com>
2021-08-14 03:20:49 +0000

    added more unit test

--- ghidraAnalysisQueue_test.go
+++ ghidraAnalysisQueue_test.go
@@ -4,8 +4,9 @@ import (
 	"testing"
 )
 
+var config, _ = NewConfiguration("test", "test", "test", "test")
+
 func TestAddToQueue(t *testing.T) {
-	config, _ := NewConfiguration("test", "test", "test", "test")
 	ghidraScriptService := NewGhidraScriptService(config)
 	binName := "testBinName"
 	script := "testScript"
@@ -13,6 +14,29 @@ func TestAddToQueue(t *testing.T) {
 	ghidraScriptService.AddToQueue(&binName, &script)
 
 	if linkedListElement := ghidraScriptService.findElement(&binName); linkedListElement == nil {
+		t.FailNow()
+	}
+
+	invalidBinName := "test"
+	if linkedListElement := ghidraScriptService.findElement(&invalidBinName); linkedListElement != nil {
+		t.FailNow()
+	}
+
+}
+
+func TestRemoveFromQueue(t *testing.T) {
+	ghidraScriptService := NewGhidraScriptService(config)
+	binName := "testBinName"
+	script := "testScript"
+
+	ghidraScriptService.AddToQueue(&binName, &script)
+
+	if linkedListElement := ghidraScriptService.findElement(&binName); linkedListElement == nil {
+		t.FailNow()
+	}
+
+	ghidraScriptService.RemoveFromQueue(&binName)
+	if linkedListElement := ghidraScriptService.findElement(&binName); linkedListElement != nil {
 		t.FailNow()
 	}
 
